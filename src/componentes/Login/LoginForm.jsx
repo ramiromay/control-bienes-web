e4215498f0c24bc2146fd25e606269caaea4b74a
Ivett <utmivett@gmail.com>
2024-07-19 16:42:03 -0600

    subiendo cambios

--- src/componentes/Login/LoginForm.jsx
+++ src/componentes/Login/LoginForm.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { FaUser, FaLock } from 'react-icons/fa'
+import '../../style'
 
 const LoginForm = () => {
   return (
-    <div>LoginForm</div>
+    <div className='wrapper'>
+        LOGINFORM
+        <form action="">
+            <h1>LOGIN</h1>
+            <div className="input-box">
+                <input type="text" placeholder='Username' required/>
+                <FaUser />
+            </div>
+            <div className="input-box">
+                <input type="password" placeholder='Password' required/>
+                <FaLock />
+            </div>
+            <div className="remember-forgot">
+                <label><input type="checkbox" />Remember me</label>
+                <a href="#">Forgot Password</a>
+            </div>
+
+            <button>Inciciar Sesión</button>
+        </form>
+    </div>
   )
 }
 
