6694d89263d92caca4075a442158662718bd873f
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-30 14:00:33 -0600

    Cambio en las dependencias del proyeco

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    include: /src\/.*\.js$/
+  },
+  resolve: {
+    alias: {
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@settings": path.resolve(__dirname, "./src/settings"),
+      "@context": path.resolve(__dirname, "./src/context"),
+      "@img": path.resolve(__dirname, "./src/img"),
+    },
+  },
 })
